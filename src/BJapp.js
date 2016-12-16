53aeed96fe9f44c86e09e6e7b998991a30a40bfd
alex diaz <alexdiaz094@gmail.com>
2016-12-16 16:11:31 -0500

    recode/newStyle

---
+++ src/BJapp.js
@@ -0,0 +1,109 @@
+function getTheSuit()
+{
+  suit = Math.floor(Math.random() * 4) + 1
+  if(suit == 1)
+{
+  return "Hearts";
+}
+if(suit == 2)
+{
+  return "Diamonds";
+}
+if(suit == 3)
+{
+  return "Spades";
+}
+if(suit == 4)
+{
+  return "Clubs";
+  }
+}
+function getACard(turn)
+{
+  card = Math.floor(Math.random() * 13) + 1
+  suit = getTheSuit();
+  if(card == 1)
+{
+  return 11;
+}
+if(card > 10)
+{
+  return 10;
+}
+
+return card;
+}
+
+var counter = 0;
+
+function GenerateNewCard()
+{
+  counter +=1;
+  var d = document.getElementById("HUM").innerHTML = "";
+  var e = document.getElementById("COMP").innerHTML = "";
+  var f = document.getElementById("COUNT").innerHTML = counter;
+  var a = parseInt(document.getElementById("dealer").value = 0);
+  var res = document.getElementById("dealer");
+  var aa = parseInt(document.getElementById("you").value - 0);
+  var resa = document.getElementById("You");
+  resa.value = a + getACard("You");
+  res.value = aa + getACard("Dealer");
+}
+
+function COMPUTER()
+{
+  var a = parseInt(document.getElementById("dealer").value);
+  var res = document.getElementById("dealer");
+  while(res.value < 17)
+  {
+  var b = parseInt(document.getElementById("dealer").value);
+  res.value = b + getACard("Dealer");
+  }
+}
+function HUMAN()
+{
+  var a = parseInt(document.getElementById("you").value);
+  var res = document.getElementById("you");
+  res.value = a + getACard("You");
+  if(res.value > 21)
+  {
+    whoWonC += 1;
+    document.getElementById("COMPW").innerHTML = whoWonC;
+    var a = document.getElementById("COMP").innerHTML = "I WIN";
+  }
+}
+var whoWonH = 0;
+var whoWonC = 0;
+
+function determineWinner()
+{
+    var a = parseInt(document.getElementById("you").value);
+    var b = parseInt(document.getElementById("dealer").value);
+    if(b > 21)
+{
+    whoWonH += 1;
+    document.getElementById("HUMW").innerHTML = whoWonH;
+    var winner = document.getElementById("HUM").innerHTML = "I WIN";
+    return;
+}
+  if(a==b)
+  {
+    var a = document.getElementById("HUM").innerHTML = "TIE";
+    var b = document.getElementById("COMP").innerHTML = "TIE";
+  }
+  if(a>b)
+  {
+    whoWonH += 1;
+    document.getElementById("HUMW").innerHTML = whoWonH;
+    var a = document.getElementById("HUMW").innerHTML = "I WIN";
+  }
+  if(b>a)
+  {
+    whoWonC += 1;
+    document.getElementById("COMPW").innerHTML = whoWonC;
+    var g = document.getElementById("COMP").innerHTML = "I WIN!";
+  }
+}
+
+
+
--- src/app.js
+++ src/app.js
@@ -9,10 +9,10 @@ return Math.round(num)+1;
 
 function getSuit(){
 suit = Shuffle(4);
-if(suit == 1) return "&Spades";
-if(suit == 2) return "&Clubs";
-if(suit == 3) return "&Diamonds";
-else return "&Hearts";
+if(suit == 1) return "Spades";
+if(suit == 2) return "Clubs";
+if(suit == 3) return "Diamonds";
+else return "Hearts";
 }
 
 function cardName(card){
@@ -24,11 +24,9 @@ return "" + card;
 }
 
 function cardValue(card,strWho){
-if(card == 1)
-{
-    if(strWho =="You" && document.gameboard.you.value > 10)
-{
-    document.gameboard.say2.value = document.gameboard.say2.value + " Low";
+if(card == 1){
+    if(strWho =="You" && document.display.you.value > 10){
+    document.display.say2.value = document.display.say2.value + " Low";
     return 1;
   } else return 11;
 }
@@ -37,41 +35,41 @@ if(card == 1)
   return card;
 }
 
-function PickACard(strWho){
+function pickACard(strWho){
 card = Shuffle(12);
 suit = getSuit();
 
-if(strWho == "You")
-document.gameboard.say2.value = (cardName(card) + " of " + suit);
-else
-document.gameboard.say1.value = (cardName(card) + " of " + suit);
+if(strWho =="You"){
+document.display.say2.value=(cardName(card) + " of " + suit);
+}else{
+document.display.say1.value=(cardName(card) + " of " + suit);
 return cardValue(card,strWho);
+  }
 }
-
 function NewHand(form){
-if(gameOver != 0)
+if(gameOver !=0)
 {
-  form.say1.value = ("Hand in Play!");
-  form.say2.value = ("");
+  form.say1.value=("Hand in Play!");
+  form.say2.value=("");
   return;
 }else{
 
   form.dealer.value = 0;
   form.you.value = 0;
   cardCount=0;
-  form.dealer.value = eval(form.dealer.value) + PickACard("Dealer");
-  form.you.value = eval(form.you.value) + PickACard("You");
+  form.dealer.value=eval(form.dealer.value) + pickACard("Dealer");
+  form.you.value=eval(form.you.value) + pickACard("You");
 
-  gameOver = -1;
-  cardCount +=1;
+  gameOver=-1;
+  cardCount+=1;
   }
 }
 
 function Dealer(form){
-if (gameOver == 0)
+if (gameOver ==0)
 {
-  form.say1.value = ("Deal the Cards!");
-  form.say2.value = ("");
+  form.say1.value=("Deal the Cards!");
+  form.say2.value=("");
   return;
 }else
 
@@ -91,25 +89,25 @@ if (cardCount < 2)
 
 while(form.dealer.value < 17)
 {
-  form.dealer.value = eval(form.dealer.value) + PickACard("Dealer");
+  form.dealer.value = eval(form.dealer.value) + pickACard("Dealer");
   }
 }
 
 function User(form){
 if (gameOver ==0)
 {
-  form.say1.value = ("Deal the Cards!");
-  form.say2.value = ("");
+  form.say1.value=("Deal the Cards!");
+  form.say2.value=("");
   return;
 }else{
 
   cardCount+=1;
-  form.say1.value = ("You Get...");
-  form.you.value = eval(form.you.value) + PickACard("You");
+  form.say1.value=("You Get...");
+  form.you.value=eval(form.you.value) + pickACard("You");
 }
 if(form.you.value > 21)
 {
-  form.say1.value = ("You Busted!");
+  form.say1.value=("You Busted!");
   gameOver=0; form.numgames.value=eval(form.numgames.value)-1;
   }
 }
@@ -145,17 +143,17 @@ if(form.you.value > form.dealer.value)
 }else{
   form.say1.value = ("House Wins!");
   form.say2.value = ("Better Luck Next Time!");
-  gameOver = 0;
-  form.numgames.value = eval(form.numgames.value) - 1;
+  gameOver =0;
+  form.numgames.value = eval(form.numgames.value) -1;
   }
 }
   function setBj(){
-  gameOver = 0;
-  cardCount = 0;
-  document.getElementById("Dealer").onclick.form.gameboard.dealer.value="";
-  document.getElementById("LookAtHands").onclick.form.gameboard.you.value="";
-  document.getElementById("User").onclick.form.gameboard.numgames.value="0";
-  document.getElementById("NewHand").onclick.form.gameboard.say1.value="    Hit 'Deal'";
-  document.getElementById("NewHand").onclick.form.gameboard.say2.value="    To Start!";
+  gameOver =0;
+  cardCount =0;
+  document.display.dealer.value="";
+  document.display.you.value="";
+  document.display.numgames.value="0";
+  document.display.say1.value="    Hit 'Deal'";
+  document.display.say2.value="    To Start!";
 }
 
